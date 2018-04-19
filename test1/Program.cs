32c39b4935aff734f0ce1c92f88a0f0ca28bbe25
jayfeather1255 <jayfeather1255@yahoo.com.tw>
2018-04-19 23:05:39 +0800

    修正身高只能輸入公尺問題，美化介面及修改註解

--- test1/Program.cs
+++ test1/Program.cs
@@ -13,36 +13,39 @@ namespace test1
             // 將視窗標題設為BMI計算機
             Console.Title = "BMI計算機";
 
-            Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Magenta;
-
             Console.WriteLine("BMI計算機"+"\n");
 
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
             // 輸入身高體重
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write("請輸入體重(公斤):");        
             string kg = Console.ReadLine();
-            Console.Write("請輸入身高(公尺):");
-            string tall = Console.ReadLine();            
-                                   
-            // 計算結果 
-            double BMI = double.Parse(kg) / (Math.Pow(double.Parse(tall), 2));
-            double result = 0;
-            result = Math.Round(BMI, 2); 
-            Console.WriteLine("\n"+"BMI值為:" + result+"\n");
+            Console.Write("請輸入身高(公分):");
+            string tall = Console.ReadLine();
 
-            // 判斷BMI
+            // 將身高單位轉換為公尺
+            double newTall = double.Parse(tall)/100;
+
+            // 計算BMI結果並顯示 
+            double BMI = Math.Round(double.Parse(kg) / newTall,2);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n"+"BMI值為:" + BMI+"\n");
+
+            // 判斷BMI是否過重或符合免兵役標準
             if (BMI < 18.5)
             {
                 if (BMI < 16.5)
-                    Console.Write("※符合免兵役標準");
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("※符合免兵役標準");
+                }                   
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("您的體重過輕!:OOOOOOOO"+"\n"+"快多吃點飯飯!");
             }                
 
             else if (BMI >= 18.5 && BMI < 24)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("您的BMI指數正常:)");                
             }
                 
@@ -56,23 +59,29 @@ namespace test1
             else if (BMI >= 27 && BMI < 30)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("您屬於輕度肥胖:(");                
+                Console.WriteLine("您屬於【輕度肥胖】:(");                
             }
 
             else if (BMI >= 30 && BMI < 35)
             {
                 if (BMI > 31.5)
-                    Console.Write("※符合免兵役標準");
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("※符合免兵役標準");
+                }                    
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("您屬於中度肥胖:((");                
+                Console.WriteLine("您屬於【中度肥胖】:((");                
             }
 
             else if (BMI >= 35)
             {
                 if (BMI > 31.5)
-                    Console.Write("※符合免兵役標準");
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("※符合免兵役標準");
+                }                    
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("您屬於重度肥胖:(((" + "\n" + "☆聰明吃-正確飲食觀：請搜尋衛生福利部國民健康署");                
+                Console.WriteLine("您屬於【重度肥胖】:(((" + "\n" + "☆聰明吃-正確飲食觀：請搜尋衛生福利部國民健康署");                
             }                    
 
             // 讀取
